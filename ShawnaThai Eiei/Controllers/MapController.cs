e657bb993ef973201d8b65e7d32644adb8f02c98
zhikazekyo <32483713+zhikazekyo@users.noreply.github.com>
2017-11-21 23:21:41 +0700

    GoogleMap

---
+++ ShawnaThai Eiei/Controllers/MapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShawnaThai_Eiei.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Map
+        public ActionResult GoogleMap()
+        {
+            return View();
+        }
+    }
+}
